75750063716c0e78ae580066aae39a7f44bf3fef
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-13 11:57:16 +0100

    Basic exercise 1.2 complete

---
+++ src/main/java/basic_1_2/CheckPassFail.java
@@ -0,0 +1,25 @@
+package basic_1_2;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class CheckPassFail {
+
+    public static void main(String[] args) {
+
+//        checkPassFail(0);
+//        checkPassFail(49);
+//        checkPassFail(50);
+//        checkPassFail(51);
+//        checkPassFail(100);
+
+    }
+
+    public static void checkPassFail(int mark) {
+
+        if (mark >= 50) {
+            System.out.println("PASS\nDONE");
+        } else System.out.println("FAIL\nDONE");
+
+    }
+
+}
