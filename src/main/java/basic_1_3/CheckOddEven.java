2d6d56314a7bec94f0f380aca0b33b2fc7237ca2
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-13 14:07:58 +0100

    Basic exercise 1.3 complete

---
+++ src/main/java/basic_1_3/CheckOddEven.java
@@ -0,0 +1,26 @@
+package basic_1_3;
+
+public class CheckOddEven {
+
+    public static void main(String[] args) {
+
+//        checkOddEven(0);
+//        checkOddEven(1);
+//        checkOddEven(88);
+//        checkOddEven(99);
+//        checkOddEven(-1);
+//        checkOddEven(-2);
+
+    }
+
+    public static void checkOddEven(int number) {
+
+        if (number % 2 == 0) {
+            System.out.println("Even Number\nbye!");
+        } else {
+            System.out.println("Odd Number\nbye!");
+        }
+
+    }
+
+}
