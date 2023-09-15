f5a4227d94ff822ae4d571fcabd3be0107347bb0
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-15 12:02:55 +0100

    OOP exercise 1.9 complete, still working to resolve final issue in 1.8 where I want to initialise the hour, minute and second fields with bounds

---
+++ src/main/java/oop_1_8/Time.java
@@ -0,0 +1,59 @@
+package oop_1_8;
+
+public class Time {
+
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public String toString() {
+        return "%02d:%02d:%02d".formatted(hour, minute, second);
+    }
+
+    public Time nextSecond() {
+        this.second += 1;
+        return new Time(getHour(), getMinute(), getSecond());
+    }
+
+    public Time previousSecond() {
+        this.second -= 1;
+        return new Time(getHour(), getMinute(), getSecond());
+    }
+
+}
