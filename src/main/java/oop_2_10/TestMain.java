9925ced1b209f3bc309060a68795aec58d14c021
Pulmonary Archery <fergus.thompson@hotmail.co.uk>
2023-09-21 12:58:55 +0100

    OOP exercise 2.10 partially complete, working out how to calculate area/perimeter of a rectangle with only two points

---
+++ src/main/java/oop_2_10/TestMain.java
@@ -0,0 +1,19 @@
+package oop_2_10;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+
+        MyRectangle r1 = new MyRectangle(15, 30, 45, 60);
+        System.out.println(r1);
+        MyRectangle r2 = new MyRectangle(new MyPoint(15, 30), new MyPoint(45, 60));
+        System.out.println(r2);
+
+        r1.setTopLeft(new MyPoint(30, 45));
+        r1.setBottomRight(new MyPoint(60, 75));
+        System.out.println("r1 topLeft is: " + r1.getTopLeft());
+        System.out.println("r1 bottomRight is: " + r1.getBottomRight());
+
+    }
+
+}
